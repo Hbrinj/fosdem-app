b75b6d4e1fca21c4b2d17425b20b497749eb64aa
houman <hbrinjcar@gmail.com>
2018-10-13 22:47:05 +0530

    Sorted and sectioned tracks list

--- fosdem/Models/Schedule.swift
+++ fosdem/Models/Schedule.swift
@@ -17,7 +17,30 @@ class Schedule {
     }
     
     func getTracks() -> [String] {
-        return Array(Set(talks.map({$0.track}))).sorted(by: {$0 < $1})
+        return Array(Set(talks.map({$0.track})))
+    }
+    
+    func getSectionedTracks() -> [Section<String>] {
+        var dict = [Character:Section<String>]()
+        var letter: Character
+        var sections = [Section<String>]()
+        
+        for track in getTracks() {
+            letter = track.uppercased().first!
+            if(dict[letter] != nil) {
+                dict[letter]?.objects.append(track)
+            } else {
+                dict[letter] = Section<String>(title: String(letter), objects: [track])
+            }
+        }
+        
+
+        for key in dict.keys {
+            dict[key]?.objects.sort(by: {(s1, s2) -> Bool in return s1.uppercased() < s2.uppercased()})
+        
+        }
+        sections = dict.compactMap({$1})
+        return sections.sorted(by: {$0.title < $1.title})
     }
     
     func getDays() -> [String] {
---
+++ fosdem/Models/Section.swift
@@ -0,0 +1,14 @@
+//
+//  Section.swift
+//  fosdem
+//
+//  Created by Houman Brinjcargorabi on 13/10/2018.
+//  Copyright © 2018 Hbrinj. All rights reserved.
+//
+
+import Foundation
+
+struct Section<T> {
+    let title: String
+    var objects: [T]
+}
